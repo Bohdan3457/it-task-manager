ba705e8c5ba59ff13b41c2b151913155fc3b2800
Bohdan3457 <bohdankhomenko04@gmail.com>
2026-07-08 19:27:11 +0200

    feat: complete index view and template with full statistics

--- task_manager/views.py
+++ task_manager/views.py
@@ -5,9 +5,13 @@ from .models import Worker, Position, Task
 
 def index(request) -> HttpResponse:
     num_task = Task.objects.count()
+    num_worker = Worker.objects.count()
+    num_position = Position.objects.count()
 
     context = {
         "num_task": num_task,
+        "num_worker": num_worker,
+        "num_position": num_position,
     }
 
     return render(request, "task_manager/index.html", context=context)
