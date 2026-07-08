bd3a3ab90319f51568d3966ba3ac1c413a35cfdd
Bohdan3457 <bohdankhomenko04@gmail.com>
2026-07-08 18:54:55 +0200

    feat: register models in admin panel with custom layouts

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Position
+from .models import Task
+from .models import TaskType
+from .models import Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+    (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "is_completed", "priority", "task_type")
+    list_filter = ("is_completed", "priority", "task_type")
+    search_fields = ("name", "description")
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
