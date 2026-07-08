60e9c7e732472877c50a3433c28dcb76a5c903f3
Bohdan3457 <bohdankhomenko04@gmail.com>
2026-07-08 17:56:58 +0200

    feat: add Position and Worker models with ForeignKey relation

---
+++ task_manager/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models import CharField
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="workers",
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.position.name if self.position else 'No position'})"
+    
